44b4b0e62d891d899ea31a65461824c24bfacebb
Rachit Shah <rachitshah2809@gmail.com>
2023-08-09 10:04:30 -0500

    Updated

--- Day-5/Ecommerce/script.js
+++ Day-5/Ecommerce/script.js
@@ -80,19 +80,19 @@ sidebarCloseNavigationEl.addEventListener("click", () => {
 
 
 //Database
-var firebaseConfig = {
-    apiKey: "AIzaSyC4lnNuY21tqXAob3ZkJmdV15A7McSuDks",
-    authDomain: "ecommerce-98194.firebaseapp.com",
-    databaseURL: "https://ecommerce-98194-default-rtdb.firebaseio.com",
-    projectId: "ecommerce-98194",
-    storageBucket: "ecommerce-98194.appspot.com",
-    messagingSenderId: "897033013069",
-    appId: "1:897033013069:web:d1a52192e57a3f0097d904",
-    measurementId: "G-QCPVDMDCGW"
-  };
+// var firebaseConfig = {
+//     apiKey: "AIzaSyC4lnNuY21tqXAob3ZkJmdV15A7McSuDks",
+//     authDomain: "ecommerce-98194.firebaseapp.com",
+//     databaseURL: "https://ecommerce-98194-default-rtdb.firebaseio.com",
+//     projectId: "ecommerce-98194",
+//     storageBucket: "ecommerce-98194.appspot.com",
+//     messagingSenderId: "897033013069",
+//     appId: "1:897033013069:web:d1a52192e57a3f0097d904",
+//     measurementId: "G-QCPVDMDCGW"
+//   };
 
 
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
 
   
 
